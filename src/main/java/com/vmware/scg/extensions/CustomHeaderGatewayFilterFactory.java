22fa2921d10d45c276c65bdfd5da62b39e0cc8cc
Abel Salgado Romero <romeroab@vmware.com>
2021-06-16 14:23:08 +0200

    Add simple blockhound example

--- src/main/java/com/vmware/scg/extensions/CustomHeaderGatewayFilterFactory.java
+++ src/main/java/com/vmware/scg/extensions/CustomHeaderGatewayFilterFactory.java
@@ -3,6 +3,9 @@ package com.vmware.scg.extensions;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.stereotype.Component;
@@ -10,13 +13,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class CustomHeaderGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
 
-	public CustomHeaderGatewayFilterFactory() {
-		System.out.println("123");
-	}
-
 	@Override
 	public GatewayFilter apply(Object config) {
 		return (exchange, chain) -> {
+
+//			Mono.just(1)
+//				.publishOn(Schedulers.parallel())
+//				.doOnNext(it -> {
+//					try {
+//						Thread.sleep(10);
+//					}
+//					catch (InterruptedException e) {
+//						throw new RuntimeException(e);
+//					}
+//				})
+//				.block();
 
 			exchange.getResponse()
 					.getHeaders()
